5efdbfe7353290e22390249e297f4da178de35d5
Piyush Hedaoo <106464462+piyushhedaoo@users.noreply.github.com>
2024-02-20 13:29:11 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        'black-dark': '#00000050',
+        'dull-white': '#FFFFFFB3',
+        'white-light': '#FFFFFF30',
+        'white-medium': '#FFFFFF40',
+        'neon-blue': '#2FB8FF',
+      }
+    },
+  },
+  plugins: [],
+}
